34ccfe6a937706b46d940f95814204ce7e73e104
Etebom Ntuk <etebom.ntuk@azubiafrica.org>
2024-12-09 17:05:33 +0100

    removed docker files

--- main.py
+++ main.py
@@ -117,3 +117,4 @@ async def process_search(data, model):
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# uvicorn main:app --host 0.0.0.0 --port $PORT
